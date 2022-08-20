2bc5f30b6e7f2b733a825c59c29338bf44e764f4
Jonathan <jonathanmoorevla@gmail.com>
2022-08-20 16:27:23 -0500

    Meet core deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    createMenu();
+    addCreateBtn();
+})
+
+const ramenData = []
+function createMenu() {
+    fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(ramen => {
+        processMenu(ramen)
+        });
+        previewRamen(1)
+    })
+}
+
+function processMenu(ramen) {
+    let ramenThumbnail = document.createElement('div')
+    ramenData.push(ramen)
+    ramenThumbnail.innerHTML = `<img id="${ramen.id}" src="${ramen.image}">`
+    ramenThumbnail.addEventListener('click', (e) => {
+        previewRamen(e.target.id);
+    })
+    let menuCont = document.getElementById('ramen-menu')
+    menuCont.appendChild(ramenThumbnail)
+
+}
+
+function previewRamen(idRam) {
+    let selectedRam = ramenData.find(ramenObj => parseInt(ramenObj.id) === parseInt(idRam))
+    document.querySelector('.detail-image').src=`${selectedRam.image}`
+    document.querySelector('.name').innerText = `${selectedRam.name}`
+    document.querySelector('.restaurant').innerText = `${selectedRam.restaurant}`
+    document.querySelector('#rating-display').innerText = `${parseInt(selectedRam.rating)}`
+    document.querySelector('#comment-display').innerText = `${selectedRam.comment}`
+}
+
+function addCreateBtn() {
+    let form = document.getElementById('new-ramen')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        addNew(e)
+        form.reset();
+    })
+}
+
+function addNew(e) {
+    let newRamenObj = {
+        "name": `${e.target.querySelector('#new-name').value}`,
+        "restaurant": `${e.target.querySelector('#new-restaurant').value}`,
+        "image": `${e.target.querySelector('#new-image').value}`,
+        "rating": `${e.target.querySelector('#new-rating').value}`,
+        "comment": `${e.target.querySelector('#new-comment').value}`
+    }
+    console.log(newRamenObj)
+    fetch('http://localhost:3000/ramens', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newRamenObj)
+    })
+    .then(resp => resp.json())
+    .then(data => processMenu(data))
+}
